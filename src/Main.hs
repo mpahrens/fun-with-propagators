c5734ffbbd97ec3fead56092dc090784ce04a53d
Matt Ahrens <matthew.ahrens@tufts.edu>
2016-03-18 11:45:41 -0400

    + example and Ed's forward and backward examples

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
 module Main where
+import Data.Propagator
+import Control.Monad.ST
+
+example1 :: Maybe Double
+example1 = runST $ do {
+  x <- cell
+; y <- cell
+; z <- cell
+; _ <- lift2 (+) x y z
+; _ <- write x 1.0
+; _ <- write y 2.0
+; content z
+}
+
+example2 :: Maybe Double
+example2 = forwards (\c -> c * 9/5 + 32) 100
+
+example3 :: Maybe Double
+example3 = backwards (\c -> c * 9/5 + 32) 212
 
 main :: IO ()
 main = putStrLn "Hello, World!"
